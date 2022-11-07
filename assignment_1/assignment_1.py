4bc1c5ddf925ddb07e589f13449d1f72d5da7e3b
Triatop <kimdenlille@gmail.com>
2022-11-07 11:18:01 +0100

    Add files via upload

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -1,5 +1,31 @@
 #https://github.com/sampepose/SpamClassifier
-# https://rstudio-pubs-static.s3.amazonaws.com/89589_4d2a66ce5276434c9f56a54df5739e85.html
-# https://www2.stat.duke.edu/courses/Spring06/sta293.3/topic9/spam_name.txt
-# https://en.wikipedia.org/wiki/Receiver_operating_characteristic
-# https://en.wikipedia.org/wiki/Confusion_matrix
+#https://rstudio-pubs-static.s3.amazonaws.com/89589_4d2a66ce5276434c9f56a54df5739e85.html
+#https://www2.stat.duke.edu/courses/Spring06/sta293.3/topic9/spam_name.txt
+#https://en.wikipedia.org/wiki/Receiver_operating_characteristic
+#https://en.wikipedia.org/wiki/Confusion_matrix
+
+import pandas as pd
+
+
+class spamDetection:
+    def __init__(self):
+        df = pd.DataFrame()
+        return
+    def readData(self, data = "spambase/spambase.header"):
+        self.df = pd.read_csv("spambase/spambase.data", header=None)
+        return
+    def setHeader(self, headerSrc = "spambase/spambase.header"):
+        header = ""
+        with open(headerSrc) as f:
+            header = f.readlines()
+        for i, _ in enumerate(header):
+            header[i] = header[i].strip()
+            header[i] = header[i].strip(',')
+        self.df.columns = header
+        return
+    def clean(self):
+        self.df.dropna()
+spamDe = spamDetection()
+spamDe.readData()
+spamDe.setHeader()
+print(spamDe.df)
