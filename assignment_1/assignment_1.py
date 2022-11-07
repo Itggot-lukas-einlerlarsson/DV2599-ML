a312089e5be087baae8bd2dedd071572ccb794b9
Lukas Einler Larsson <Lukas.einlerlarsson@itggot.se>
2022-11-07 11:12:59 +0100

    added dirs

---
+++ assignment_1/assignment_1.py
@@ -0,0 +1,5 @@
+#https://github.com/sampepose/SpamClassifier
+# https://rstudio-pubs-static.s3.amazonaws.com/89589_4d2a66ce5276434c9f56a54df5739e85.html
+# https://www2.stat.duke.edu/courses/Spring06/sta293.3/topic9/spam_name.txt
+# https://en.wikipedia.org/wiki/Receiver_operating_characteristic
+# https://en.wikipedia.org/wiki/Confusion_matrix
