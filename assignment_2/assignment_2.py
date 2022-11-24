a6c6387c7c4dbc587a3c9e33dcf5ddca5a2db601
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2022-11-24 12:46:19 +0100

    dict holds np.arrays and tested knn

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
 class spamDetection:
     def __init__(self):
@@ -34,21 +36,31 @@ class spamDetection:
             instance = self.df.iloc[i].to_list()
             data.append(instance[:-1])
             target.append(instance[-1])
-        spamDict["data"] = data
-        spamDict["target"] = target
-        spamDict["target_names"] = ["spam", "ham"]
-        spamDict["feature_names"] = self.df.columns.to_list()[:-1]
-        # self.spamDict = spamDict
-        return spamDict
+        spamDict["data"] = np.array(data)
+        spamDict["target"] = np.array(target)
+        spamDict["target_names"] = np.array(["spam", "ham"])
+        spamDict["feature_names"] = np.array(self.df.columns.to_list()[:-1])
+        self.spamDict = spamDict
 
+    def run_kNNclassifier(self, n_neighbors = 5):
+        X, y = self.spamDict["data"], self.spamDict["target"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        print(X_train.shape, X_test.shape)
+        knn = KNeighborsClassifier(n_neighbors)
+        knn.fit(X_train, y_train)
+        print("Accuracy:", knn.score(X_test, y_test))
+        y_pred = knn.predict(X_test)
+        print("{0} / {1} correct".format(np.sum(y_test == y_pred), len(y_test)))
+        print(confusion_matrix(y_test, y_pred))
 
 def main():
     spamDe = spamDetection()
     spamDe.readData()
     spamDe.setHeader()
     spamDe.clean()
-    dict = spamDe.datasetCreation()
-    print(dict["feature_names"])
+    spamDe.datasetCreation()
+    spamDe.run_kNNclassifier()
+
 
 if __name__ == '__main__':
     main()
