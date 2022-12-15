4d7996fd5d6e5da08f6f6a658527b34890c4c61b
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2022-12-15 14:42:04 +0100

    cluster all instances, index all fraud instances and then check their clusters

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
+# from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 sns.set_theme()
 
@@ -27,19 +31,40 @@ class fraudDetection:
     def clean(self):
         self.df.dropna()
         self.df = self.df.drop_duplicates()
-        # self.df = self.df.drop("Class")
-        print(self.df.columns)
+        self.cleaneDf = self.df.drop(["Class"], axis = 1)
+        self.cleaneDf = self.cleaneDf.drop(["Time"], axis = 1)
+        self.cleaneDf = self.cleaneDf.drop(["Amount"], axis = 1)
+        print(self.cleaneDf.columns)
 
-    def testKMeans(self):
-        kMeans = KMeans(n_clusters=3, random_state=0)
-        kMeans.fit(self.df["v1"].to_numpy())
+    def testClusterModels(self, n = 7):
+        """
+            Testar runt med clustering algorithmer, Just nu clustrar den alla instanser, tar fram index för varje fraud instans,
+            och printar ut vilket kluster den instansen fick
+
+            todo: första parametrar o testa runt o försöka få alla fraud instanser till samma kluster.
+        """
+        # kMeans = KMedoids(n_clusters=10, random_state=0)
+        kMeans = KMeans(n_clusters= n, n_init=10) # går snabbt
+        # kMeans = DBSCAN(eps=0.5, min_samples=5) # långsam men funka tog ca 5 min
+        # kMeans = SpectralClustering(n_clusters=n, assign_labels='discretize', random_state=0) # Unable to allocate 600. GiB for an array with shape (283726, 283726) and data type float64
+        # kMeans = AgglomerativeClustering() #Unable to allocate 300. GiB for an array with shape (40250079675,) and data type float64
+        kMeans.fit(self.cleaneDf.to_numpy())
+        fraud_labels = []
+        fraudIndices = self.df[self.df["Class"] == 1.0].index.to_numpy()
+        for index in fraudIndices:
+            fraud_labels.append(kMeans.labels_[index])
+        print(fraud_labels)
+
+    def plotFraudData(self, n = 7):
+        kMeans = KMeans(n_clusters= n, n_init=10)
+        kMeans.fit(self.df[self.df["Class"] == 1.0].to_numpy())
         pca = PCA(n_components=2)
-        pca.fit(self.df["v1"].to_numpy())
-        X_reduced = pca.transform(self.df.to_numpy())
+        pca.fit(self.df[self.df["Class"] == 1.0].to_numpy())
+        X_reduced = pca.transform(self.df[self.df["Class"] == 1.0].to_numpy())
         plt.scatter(X_reduced[:, 0],
             X_reduced[:, 1],
             c=kMeans.labels_,
-            cmap=plt.cm.get_cmap('spring', 3),
+            cmap=plt.cm.get_cmap('spring', n),
             alpha=0.5);
         plt.xlabel('Component 1')
         plt.ylabel('Component 2')
@@ -52,7 +77,8 @@ def main():
     fraudDe.readData()
     fraudDe.setHeader()
     fraudDe.clean()
-    fraudDe.testKMeans()
+    fraudDe.testClusterModels()
+    # fraudDe.plotFraudData()
 
 
 
