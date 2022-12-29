e87a8652e9a09e8d5d0739a101f7a4ab0fdf0f31
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2022-12-29 18:47:00 +0100

    cleaned code and added downsampling option

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -4,15 +4,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cluster import KMeans
-# from sklearn_extra.cluster import KMedoids
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import SpectralClustering
 from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import Normalizer
+from sklearn import preprocessing
+from sklearn.utils import resample
 sns.set_theme()
-
 
 class fraudDetection:
     def __init__(self):
@@ -31,32 +26,12 @@ class fraudDetection:
         self.df.columns = header
         return
     def clean(self):
-        # self.df.dropna()
-        # self.df = self.df.drop_duplicates()
+        self.df.dropna()
+        self.df = self.df.drop_duplicates()
         self.cleaneDf = self.df.drop(["Class"], axis = 1)
         self.cleaneDf = self.cleaneDf.drop(["Time"], axis = 1)
         self.cleaneDf = self.cleaneDf.drop(["Amount"], axis = 1)
         print(self.cleaneDf.columns)
-
-    def preprocess(self):
-        pass
-        # tf = Normalizer()
-        # self.cleaneDf = self.cleaneDf.round(3)
-        # tf.transform(self.cleaneDf)
-        # print(self.cleaneDf)
-
-    def testClusterModels(self, n = 7):
-        """
-            Testar runt med clustering algorithmer, Just nu clustrar den alla instanser, tar fram index för varje fraud instans,
-            och printar ut vilket kluster den instansen fick
-
-            todo: första parametrar o testa runt o försöka få alla fraud instanser till samma kluster.
-
-            DBSCAN desc: https://towardsdatascience.com/how-dbscan-works-and-why-should-i-use-it-443b4a191c80
-        """
-        dbscanModel = DBSCAN(eps = 5, min_samples=10) # långsam men funka tog ca 5 min
-        dbscanModel.fit(self.cleaneDf[:20000].to_numpy())
-        self.test(dbscanModel.labels_)
 
     def plotKDistanceGraph(self, X, k):
         """
@@ -72,32 +47,55 @@ class fraudDetection:
                         ((X[i,1] - X[j,1]) ** 2)))
             eucl_dist.sort()
             kn_distance.append(eucl_dist[k])
-        plt.hist(kn_distance, bins = 50)
+        plt.hist(kn_distance, bins = 30)
         plt.ylabel('n')
         plt.xlabel('Epsilon distance')
         plt.show()
 
-
-    def plotFraudData(self, n = 7):
-        kMeans = KMeans(n_clusters= n, n_init=10)
-        kMeans.fit(self.df[self.df["Class"] == 1.0].to_numpy())
-        pca = PCA(n_components=2)
-        pca.fit(self.df[self.df["Class"] == 1.0].to_numpy())
-        X_reduced = pca.transform(self.df[self.df["Class"] == 1.0].to_numpy())
-        plt.scatter(X_reduced[:, 0],
-            X_reduced[:, 1],
-            c=kMeans.labels_,
-            cmap=plt.cm.get_cmap('spring', n),
-            alpha=0.5);
-        plt.xlabel('Component 1')
-        plt.ylabel('Component 2')
-        plt.show()
-
-    def test(self, labels):
+    def downSample(self, length = 10000):
         """
-            test accuracy of clustering model
+            Randomly extract instances that are benign
+            then concat the fraud instances to the dataframe
+        """
+        self.downsampledData = resample(self.df[self.df["Class"] == 0.0],
+                     replace=True,
+                     n_samples=length)
+                     # random_state=420) #if same random set is wanted over multiple runs
+        self.downsampledData = pd.concat([self.downsampledData, self.df[self.df["Class"] == 1.0]],axis=0)
+        # self.downsampledData = self.downsampledData.drop(["Class"], axis = 1)
+        self.downsampledData = self.downsampledData.drop(["Time"], axis = 1)
+        self.downsampledData = self.downsampledData.drop(["Amount"], axis = 1)
 
-            Labels DBscan -- -1 = outlier, X
+    def preprocess(self):
+        """
+            https://scikit-learn.org/stable/auto_examples/preprocessing/plot_all_scaling.html
+            Robust scaler is not sensitive to outliers but not better anyway
+        """
+        pass
+        # rs = preprocessing.RobustScaler()
+        # tempArray = rs.fit_transform(self.cleaneDf) # returns numpy array
+        # self.cleaneDf = pd.DataFrame(tempArray, index = self.cleaneDf.index, columns = self.cleaneDf.columns)
+
+    def testClusterModel(self, distance, minSampleSize, data):
+        """
+            Test: ändra parametrar o försöka få alla fraud instanser till samma outlier kluster och
+            samtidigt få alla benign transactions till andra kluster.
+
+            DBSCAN desc: https://towardsdatascience.com/how-dbscan-works-and-why-should-i-use-it-443b4a191c80
+        """
+        dbscanModel = DBSCAN(eps = distance, min_samples=minSampleSize)
+        if "Class" in data.columns: # if running with downsampledData and all fraud instances
+            dbscanModel.fit(data.loc[:, data.columns != "Class"])
+            self.test(dbscanModel.labels_, data)
+        else: # if running with cleaneDf[:length]
+            dbscanModel.fit(data)
+            self.test(dbscanModel.labels_, self.df)
+
+    def test(self, labels, data):
+        """
+            Test accuracy of clustering model
+
+            Labels DBscan: -1 = outlier, all other numbers belongs to cluster
         """
         print("-"*20)
         TN = 0
@@ -105,17 +103,16 @@ class fraudDetection:
         TP = 0
         FP = 0
         for i, v in enumerate(labels):
-            lst = self.df.iloc[i].to_list()
-            # print(lst)
-            # print("index", i, "\tvalue:", v)
+            instance = data.iloc[i]
+            # print(instance)
             # break
             if v != -1: # if instance is not outlier and fraud
-                if lst[-1] == 0.0: #if not actual fraud
+                if instance[-1] == 0.0: #if not actual fraud
                     TN += 1
                 else:
                     FN += 1
             else: # if outlier
-                if lst[-1] == 1.0: #if actual fraud
+                if instance[-1] == 1.0: #if actual fraud
                     TP += 1
                 else:
                     FP += 1
@@ -143,17 +140,15 @@ class fraudDetection:
         print(f"\t\t\t{TP+FP}\t\t{FN+TN}\t\t{TP+TN+FN+FP}")
         print("-"*60)
 
-
 def main():
     fraudDe = fraudDetection()
     fraudDe.readData()
     fraudDe.setHeader()
     fraudDe.clean()
+    fraudDe.downSample(length = 20000)
     fraudDe.preprocess()
-    fraudDe.testClusterModels()
-    # fraudDe.plotKDistanceGraph(fraudDe.cleaneDf[1:2000].to_numpy(), 5)
-    # fraudDe.plotFraudData()
-
-
+    fraudDe.testClusterModel(distance = 5, minSampleSize = 10, data = fraudDe.downsampledData)
+    # fraudDe.testClusterModel(distance = 5, minSampleSize = 10, data = fraudDe.cleaneDf[:20000])
+    # fraudDe.plotKDistanceGraph(fraudDe.cleaneDf[:1000].to_numpy(), 20)
 
 main()
